e5de300421a7421e647dc36d34f895fab4eadea6
irimescu-maria <irimescu.mariaalexandra@gmail.com>
2018-12-13 08:56:48 +0200

    Implementing the functionality for authentication with roles

---
+++ RentalCars/RentalCars/Controllers/RoleController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RentalCars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalCars.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public RoleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var roles = _context.Roles.ToList();
+            return View();
+        }
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+
+                var userManager = new UserManager<ApplicationUser>
+                    (new UserStore<ApplicationUser>(_context));
+
+                var s = userManager.GetRoles(user.GetUserId());
+
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ RentalCars/RentalCars/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RentalCars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalCars.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        
+
+        public UsersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+                ViewBag.displayMenu = "No";
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View();
+        }
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+
+                var userManager = new UserManager<ApplicationUser>
+                    (new UserStore<ApplicationUser>(_context));
+
+                var s = userManager.GetRoles(user.GetUserId());
+
+                if(s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
