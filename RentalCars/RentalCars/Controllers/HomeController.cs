c1bc5b7e87ffb2df8ab1a6648aed326d3b1166fb
irimescu-maria <irimescu.mariaalexandra@gmail.com>
2018-12-14 10:34:23 +0200

    Changing views

--- RentalCars/RentalCars/Controllers/HomeController.cs
+++ RentalCars/RentalCars/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RentalCars.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,24 @@ namespace RentalCars.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            if (isAdminUser())
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+           
         }
 
         public ActionResult About()
@@ -25,6 +43,25 @@ namespace RentalCars.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
         }
     }
 }
